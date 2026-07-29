2b83b764070d29a4ea89595d286b1a46cc060889
vjbabu-in <vijaykumarjonnakuti0@gmail.com>
2026-07-29 21:38:35 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,15 @@
 setTimeout(function(){
     document.getElementById("login").style.display="block";
 },4000);
+setTimeout(() => {
+    const login = document.getElementById("login");
+    login.style.display = "block";
+
+    setTimeout(() => {
+        login.classList.add("show");
+    }, 100);
+}, 4000);
+
+function login(){
+    window.location.href="dashboard.html";
+}
