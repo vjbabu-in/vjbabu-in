9bc38a7967908877d73aa6b56ccd44e4b9d552a6
vjbabu-in <vijaykumarjonnakuti0@gmail.com>
2026-07-29 21:45:01 +0530

    Update login.js

--- login.js
+++ login.js
@@ -13,3 +13,17 @@ setTimeout(() => {
 function login(){
     window.location.href="dashboard.html";
 }
+function loginUser() {
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if(username === "vijay" && password === "1234"){
+      alert("✅ Login Successful");
+      window.location.href = "dashboard.html";
+  } else {
+      alert("❌ Invalid Username or Password");
+  }
+
+  return false;
+}
