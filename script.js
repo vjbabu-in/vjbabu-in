1ab9c1d345c0fb7eecaf61db7970c6cca880f9c4
Jonnakuti vijay <vijaykumarjonnakuti0@gmail.com>
2026-09-07 08:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,34 @@ behavior:"smooth"
 });
 
 };
+// Welcome Message
+window.onload = function () {
+    console.log("Welcome to Vijay Learning Hub 🚀");
+};
+
+
+// Smooth Scroll
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        if(this.hash !== "") {
+            e.preventDefault();
+
+            let section = document.querySelector(this.hash);
+            section.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+
+// Explore Courses Button
+function exploreCourses() {
+    window.location.href = "courses.html";
+}
+
+
+// Loading Animation
+setTimeout(() => {
+    document.body.style.opacity = "1";
+}, 500);
